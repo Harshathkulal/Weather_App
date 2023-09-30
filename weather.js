4ffc414d8f93863694f9a258ff3786c4fade4e05
harshath <harshathmkulal@email.com>
2023-09-30 15:18:35 +0530

    Done with Mobile view with Boostrap

--- weather.js
+++ weather.js
@@ -7,56 +7,49 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 //Async function to fetch the data through Api
 async function checkweather(city) {
-	const response = await fetch(url + city + `&appid=${apiKey}`);
-// condition for Empty city if searched
-	if (response.status == 404) {
-		document.querySelector(".error").style.display ="block"
-		document.querySelector(".weather").style.display ="none"
-		document.querySelector(".valid").style.display ="none"
-	}
-//condition  for invalid city Name
-	else if (response.status == 400) {
-		document.querySelector(".error").style.display ="none"
-		document.querySelector(".weather").style.display ="none"
-		document.querySelector(".valid").style.display ="block"
-	}
-	else {
+  const response = await fetch(url + city + `&appid=${apiKey}`);
+  // condition for Empty city if searched
+  if (response.status == 404) {
+    document.querySelector(".error").classList.remove("d-none");
+    document.querySelector(".weather").classList.add("d-none");
+    document.querySelector(".valid").classList.add("d-none");
+  }
+  //condition  for invalid city Name
+  else if (response.status == 400) {
+    document.querySelector(".error").classList.add("d-none");
+    document.querySelector(".weather").classList.add("d-none");
+    document.querySelector(".valid").classList.remove("d-none");
+  } else {
+    const data = await response.json();
+    //Data=data
 
-		const data = await response.json();
-		console.log(data);
+    //Target all element in the screen
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
 
-//Target all element in the screen
-		document.querySelector(".city").innerHTML = data.name;
-		document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-		document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-		document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "./images/clouds-fill.svg";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/sun-fill.svg";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/cloud-drizzle-fill.svg";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/cloud-rain-fill.svg";
+    } else if (data.weather[0].main == "Snow") {
+      weatherIcon.src = "images/cloud-snow-fill.svg";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/cloud-sun-fill.svg";
+    }
 
-		if (data.weather[0].main == "Clouds") {
-			weatherIcon.src = "images/clouds.png";
-		}
-		else if (data.weather[0].main == "Clear") {
-			weatherIcon.src = "images/clear.png";
-		}
-		else if (data.weather[0].main == "Drizzle") {
-			weatherIcon.src = "images/drizzle.png";
-		}
-		else if (data.weather[0].main == "Rain") {
-			weatherIcon.src = "images/rain.png";
-		}
-		else if (data.weather[0].main == "Snow") {
-			weatherIcon.src = "images/snow.png";
-		}
-		else if (data.weather[0].main == "Mist") {
-			weatherIcon.src = "images/mist.png";
-		}
-
-		document.querySelector(".weather").style.display = "block";
-		document.querySelector(".error").style.display = "none"
-		document.querySelector(".valid").style.display ="none"
-	}
+    document.querySelector(".weather").classList.remove("d-none");
+    document.querySelector(".error").classList.add("d-none");
+    document.querySelector(".valid").classList.add("d-none");
+  }
 }
 
 btn.addEventListener("click", () => {
-	checkweather(searchbox.value);
-
-})
+  checkweather(searchbox.value);
+});
